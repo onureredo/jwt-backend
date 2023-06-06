4707eb23abf4375a26912632d1e3f80a16424b97
Onur Eredo <onureredo@yahoo.com>
2023-06-06 20:13:33 +0200

    post model

---
+++ models/Post.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+const { Schema, model, ObjectId } = mongoose;
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+  },
+  author: {
+    type: ObjectId,
+    ref: 'User',
+    required: [true, 'Author is required'],
+  },
+  image: {
+    type: String,
+    required: [true, 'Image is required'],
+  },
+  content: {
+    type: String,
+    required: [true, 'Content is required'],
+  },
+  date: { type: Date, default: Date.now },
+});
+
+export default model('Post', postSchema);
