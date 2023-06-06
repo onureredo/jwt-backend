3c8186808a70e4c1ab47ac6febc475115b8ddce5
Onur Eredo <onureredo@yahoo.com>
2023-06-06 21:22:16 +0200

    fixed posts controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -8,7 +8,7 @@ export const getAllPosts = asyncHandler(async (req, res, next) => {
 });
 
 export const createPost = asyncHandler(async (req, res, next) => {
-  const { uid } = req.body;
+  const { body, uid } = req;
   const newPost = await (
     await Post.create({ ...body, author: uid })
   ).populate('author');
@@ -16,7 +16,9 @@ export const createPost = asyncHandler(async (req, res, next) => {
 });
 
 export const getSinglePost = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
+  const {
+    params: { id },
+  } = req;
   const post = await Post.findById(id).populate('author');
   if (!post)
     throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
@@ -24,23 +26,32 @@ export const getSinglePost = asyncHandler(async (req, res, next) => {
 });
 
 export const updatePost = asyncHandler(async (req, res, next) => {
-  const { uid, id } = req.params;
-  const existingPost = await Post.findById(id);
-  if (uid !== existingPost.author.toString())
-    throw new ErrorResponse('You have no permission to update this post', 401);
+  const {
+    body,
+    params: { id },
+    uid,
+  } = req;
+  const found = await Post.findById(id);
+  if (!found)
+    throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
+  if (uid !== found.author.toString())
+    throw new ErrorResponse(`You have no permission to update this post`, 401);
   const updatedPost = await (
     await Post.findOneAndUpdate({ _id: id }, body, { new: true })
   ).populate('author');
-  res.json(updatePost);
+  res.json(updatedPost);
 });
 
 export const deletePost = asyncHandler(async (req, res, next) => {
-  const { id, uid } = req.params;
-  const existingPost = await Post.findById(id);
+  const {
+    params: { id },
+    uid,
+  } = req;
+  const found = await Post.findById(id);
   if (!found)
-    throw new ErrorResponse(`Post with id of ${id} doesn't exist, 404`);
-  if (uid !== existingPost.author.toString())
-    throw new ErrorResponse('You have no permission to delete this post', 401);
+    throw new ErrorResponse(`Post with id of ${id} doesn't exist`, 404);
+  if (uid !== found.author.toString())
+    throw new ErrorResponse(`You have no permission to delete this post`, 401);
   await Post.deleteOne({ _id: id });
   res.json({ success: `Post with id of ${id} was deleted` });
 });
